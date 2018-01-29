58e66126d861bbf600c3d94af360798ea344c9b6
Chris Whong <chris@whong.org>
2018-01-29 13:28:06 -0500

    add proxy for all other pelias endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var autocomplete = require('./autocomplete');
 var search = require('./search');
+var peliasProxy = require('../util/pelias-proxy');
 
 var router = express.Router();
 
 router.use('/v1/autocomplete', autocomplete);
 router.use('/v1/search', search);
+
+// proxy all other requests
+router.use('/v1', (req, res) => {
+  peliasProxy(req)
+    .then((data) => {
+      if (!data.errors) res.status(400)
+      res.send(data);
+    });
+})
 
 module.exports = router;
